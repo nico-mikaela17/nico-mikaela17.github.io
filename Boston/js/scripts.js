057f072f93fbd8f35692e974f1477806fae7a0b9
Nicole Linnarz <mikaelafs17@gmail.com>
2022-11-23 18:39:27 -0700

    inserted slider

--- Boston/js/scripts.js
+++ Boston/js/scripts.js
@@ -6,3 +6,17 @@ hb.addEventListener('click',() => {
     pn.classList.toggle('open'); 
 }
 );
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed:2000,
+    loop: true,
+    autoplay:{
+      delay:5000,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+})
